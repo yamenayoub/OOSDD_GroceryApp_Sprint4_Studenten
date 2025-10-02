ea6587a0235421fbc14abbac38089a15934544fe
yamen <s1211665@student.windesheim.nl>
2025-10-02 14:17:59 +0200

    Refactor UI and implement best-selling products logic
    Refactored `BestSellingProductsView.xaml` for improved readability:
    - Reorganized `ContentPage` attributes and reformatted elements.
    - Added `EmptyView` to `CollectionView` for empty product lists.
    - Enhanced `CollectionView` with headers and data bindings.
    
    Implemented `GetBestSellingProducts` in `GroceryListItemsService.cs`:
    - Retrieves and ranks top `X` best-selling products.
    - Handles empty repositories and invalid `topX` values gracefully.
    - Fetches product details and assigns rankings.
    
    Made minor namespace and import adjustments for consistency.

--- Grocery.Core/Services/GroceryListItemsService.cs
+++ Grocery.Core/Services/GroceryListItemsService.cs
@@ -1,6 +1,7 @@
 ﻿using Grocery.Core.Interfaces.Repositories;
 using Grocery.Core.Interfaces.Services;
 using Grocery.Core.Models;
+using System.Xml.Linq;
 
 namespace Grocery.Core.Services
 {
@@ -51,7 +52,32 @@ namespace Grocery.Core.Services
 
         public List<BestSellingProducts> GetBestSellingProducts(int topX = 5)
         {
-            throw new NotImplementedException();
+            // If the list is empty, or the ranking is zero, return empty bestSellingProducts list.
+            var allItems = _groceriesRepository.GetAll();
+            var bestSellingProducts = new List<BestSellingProducts>();
+            if (allItems.Count == 0 || topX <= 0)
+            {
+                return [];
+            }
+
+            // _productRepository.Get(p.ProductId).Stock  
+            bestSellingProducts = allItems
+                .OrderByDescending(p => p.Amount)
+                .Take(topX)
+                .Select((bp, index) =>
+                {
+                    var product = _productRepository.Get(bp.ProductId);
+                    ArgumentNullException.ThrowIfNull(product);
+
+                    return new BestSellingProducts(
+                        bp.ProductId,
+                        product.Name,
+                        product.Stock,
+                        bp.Amount,
+                        index + 1
+                    );
+                }).ToList();
+            return bestSellingProducts;
         }
 
         private void FillService(List<GroceryListItem> groceryListItems)
